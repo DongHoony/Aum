f3a0f0b1f8b156e75bf9d5f8075da3031793db82
DongHoony <31026350+DongHoony@users.noreply.github.com>
2018-12-13 15:16:10 +0900

    Update containui.py

--- containui.py
+++ containui.py
@@ -1,73 +1,85 @@
 # -*- coding: utf-8 -*-
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QPushButton
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QPushButton, QWidget
 
 class Aum(QWidget):
     def setupUi(self, Form):
         Form.setObjectName("Form")
-        Form.resize(580, 210)
+        Form.setFixedSize(700, 210)
+
         self.placeButton = QPushButton(Form)
-        self.placeButton.setGeometry(QtCore.QRect(370, 180, 75, 23))
+        self.placeButton.setGeometry(QtCore.QRect(490, 180, 75, 23))
         self.placeButton.setObjectName("placeButton")
         self.placeButton.clicked.connect(self.buildList)
 
         self.exitButton = QtWidgets.QPushButton(Form)
-        self.exitButton.setGeometry(QtCore.QRect(480, 180, 75, 23))
+        self.exitButton.setGeometry(QtCore.QRect(600, 180, 75, 23))
         self.exitButton.setObjectName("exitButton")
-        self.exitButton.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        self.exitButton.clicked.connect(self.openFile)
+        self.exitButton.setEnabled(False)
 
         self.groupnumSpinbox = QtWidgets.QSpinBox(Form)
-        self.groupnumSpinbox.setGeometry(QtCore.QRect(510, 20, 42, 22))
+        self.groupnumSpinbox.setGeometry(QtCore.QRect(630, 20, 42, 22))
         self.groupnumSpinbox.setObjectName("groupnumSpinbox")
 
         self.textEdit = QtWidgets.QTextEdit(Form)
-        self.textEdit.setGeometry(QtCore.QRect(370, 80, 181, 31))
+        self.textEdit.setGeometry(QtCore.QRect(490, 80, 181, 31))
         self.textEdit.setObjectName("textEdit")
 
         self.label = QtWidgets.QLabel(Form)
-        self.label.setGeometry(QtCore.QRect(450, 20, 56, 21))
+        self.label.setGeometry(QtCore.QRect(580, 20, 56, 21))
         self.label.setObjectName("label")
 
         self.label_2 = QtWidgets.QLabel(Form)
-        self.label_2.setGeometry(QtCore.QRect(370, 50, 181, 21))
+        self.label_2.setGeometry(QtCore.QRect(490, 55, 181, 21))
         self.label_2.setObjectName("label_2")
 
         self.textBrowser = QtWidgets.QTextBrowser(Form)
-        self.textBrowser.setGeometry(QtCore.QRect(20, 10, 301, 101))
+        self.textBrowser.setGeometry(QtCore.QRect(20, 10, 421, 193))
         self.textBrowser.setObjectName("textBrowser")
-
-        self.textBrowser_2 = QtWidgets.QTextBrowser(Form)
-        self.textBrowser_2.setGeometry(QtCore.QRect(20, 120, 301, 81))
-        self.textBrowser_2.setObjectName("textBrowser_2")
+        self.textBrowser.append("====== 아름다운배움 꿈사다리학교 멘토 임시배치 프로그램 ======")
+        self.textBrowser.append("같은 폴더에 aumpeople.xlsx 파일 존재 여부를 확인 후 배치해주세요.")
 
         self.progressBar = QtWidgets.QProgressBar(Form)
-        self.progressBar.setGeometry(QtCore.QRect(370, 130, 181, 23))
+        self.progressBar.setGeometry(QtCore.QRect(490, 130, 181, 23))
         self.progressBar.setProperty("value", 0)
         self.progressBar.setObjectName("progressBar")
 
         self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
+        Form.setWindowTitle(_translate("Form", "꿈사다리학교 멘토배치 프로그램"))
         self.placeButton.setText(_translate("Form", "배치"))
-        self.exitButton.setText(_translate("Form", "종료"))
+        self.exitButton.setText(_translate("Form", "파일 열기"))
         self.label.setText(_translate("Form", "그룹 수"))
         self.label_2.setText(_translate("Form", "그룹별 정원 수, \'/\'로 구분"))
 
     def buildList(self):
-        text = self.textEdit.toPlainText()
-        r_max_people = list(map(int, text.split('/')))
-        numberofgroup = self.groupnumSpinbox.value()
-        print(numberofgroup)
-        print(r_max_people)
+        global name, ALL_MAN_RATIO
         import openpyxl
         from collections import Counter
 
-        f = openpyxl.load_workbook(r"aumpeople.xlsx")
-        sheet = f.active
+        while 1:
+            text = self.textEdit.toPlainText()
+            try:
+                r_max_people = list(map(int, text.split('/')))
+            except ValueError:
+                self.textBrowser.append("잘못된 형식입니다. 다시 입력해 주세요.")
+                return 0
+            numberofgroup = self.groupnumSpinbox.value()
+            print(numberofgroup)
+            if len(r_max_people) != int(numberofgroup) or int(numberofgroup) == 0:
+                self.textBrowser.append("그룹 수와 그룹별 인원이 일치하지 않습니다. 다시 입력해 주세요.")
+                return 0
+            else:
+                break
+        try:
+            f = openpyxl.load_workbook(r"aumpeople.xlsx")
+            sheet = f.active
+        except FileNotFoundError:
+            self.textBrowser.append("파일이 존재하지 않습니다. 확인 후 배치해 주세요.")
+            return 0
 
         # 성비 확인, list형태로 내보낸다, list 안에는 tuple 형태로 (W : M).
         def RatioCheck():
@@ -113,7 +125,7 @@ class Aum(QWidget):
         w = genders.count('여자')
         m = genders.count('남자')
         ALL_MAN_RATIO = round(m * 100 / (w + m), 1)
-        print("\n총 인원 : {}명, 전체 성비 -> W {} : M {}\n".format(len(name), 100 - ALL_MAN_RATIO, ALL_MAN_RATIO))
+        self.textBrowser.append("\n총 인원 : {}명, 전체 성비 -> W {} : M {}\n".format(len(name), 100 - ALL_MAN_RATIO, ALL_MAN_RATIO))
 
         # 각 배열에 그룹 입력
         NUMBERS_OF_REGIONS = int(numberofgroup)
@@ -121,21 +133,30 @@ class Aum(QWidget):
         temp = []
         reg = REGIONS_ALPHABET[:NUMBERS_OF_REGIONS]
         for i in name:
-            regions[reg.index(i[3])].append(i)
+            try:
+                regions[reg.index(i[3])].append(i)
+            except ValueError:
+                self.textBrowser.append("입력하신 그룹 수와 엑셀 내 그룹 수와 일치하지 않습니다. 확인해 주세요.")
+                return 0
+
+        self.placeButton.setEnabled(False)
+        self.textEdit.setEnabled(False)
+        self.groupnumSpinbox.setEnabled(False)
+        self.exitButton.setEnabled(False)
 
         _ = 0
         while 1:  # While
+            self.progressBar.setProperty("value", _)
             _ += 1
-            print("ATTEMPT {}".format(_))
+            self.textBrowser.append("ATTEMPT {}".format(_))
             ppl = PersonCheck()
             isChanged = 0
             for i in range(NUMBERS_OF_REGIONS):
                 g = reg[i]
                 if ppl[i] == 0:
-                    print('OK with {}, continue...'.format(g))
-
+                    self.textBrowser.append('OK with {}, continue...'.format(g))
                 elif ppl[i] > 0:
-                    print('Too many people in {}, pushing people to temporary list...'.format(g))
+                    self.textBrowser.append('Too many people in {}, pushing people to temporary list...'.format(g))
                     for j in range(ppl[i]):
                         for a in regions[i]:
                             cnt = Counter(list(map(list, zip(*regions[i])))[2])
@@ -143,59 +164,60 @@ class Aum(QWidget):
                             manratio = GetManRatio()
                             if manratio < ALL_MAN_RATIO:
                                 if a[1] == '여자' and a[2] == most_age and a[6] == '신규':
-                                    print("Pushing {} into temp...".format(a[0]))
+                                    self.textBrowser.append("Pushing {} into temp...".format(a[0]))
                                     temp.append(a)
                                     regions[i].pop(regions[i].index(a))
                                     isChanged = 1
                                     break
                             else:
                                 if a[1] == '남자' and a[2] == most_age and a[6] == '신규':
-                                    print("Pushing {} into temp...".format(a[0]))
+                                    self.textBrowser.append("Pushing {} into temp...".format(a[0]))
                                     temp.append(a)
                                     regions[i].pop(regions[i].index(a))
                                     isChanged = 1
                                     break
                     if not isChanged:
-                        print('Nobody matches to pull out, making the gender ratio ideally...')
+                        ppl = PersonCheck()
+                        self.textBrowser.append('Nobody matches to pull out, making the gender ratio ideally...')
                         for j in range(ppl[i]):
                             for a in regions[i]:
                                 manratio = GetManRatio()
                                 if manratio < ALL_MAN_RATIO:
                                     if a[1] == '여자' and a[6] == '신규':
-                                        print("Pushing {} into temp...".format(a[0]))
+                                        self.textBrowser.append("Pushing {} into temp...".format(a[0]))
                                         temp.append(a)
                                         regions[i].pop(regions[i].index(a))
                                         isChanged = 1
                                         break
                                 else:
                                     if a[1] == '남자' and a[6] == '신규':
-                                        print("Pushing {} into temp...".format(a[0]))
+                                        self.textBrowser.append("Pushing {} into temp...".format(a[0]))
                                         temp.append(a)
                                         regions[i].pop(regions[i].index(a))
                                         isChanged = 1
                                         break
                 else:
-                    print('Low people in {}, pulling people from temporary list...'.format(g))
+                    self.textBrowser.append('Low people in {}, pulling people from temporary list...'.format(g))
                     if not temp:
-                        print('No people in temporary list, continue...'.format(g))
+                        self.textBrowser.append('No people in temporary list, continue...'.format(g))
                     else:
                         for a in temp:
                             manratio = GetManRatio()
                             if manratio < ALL_MAN_RATIO:
                                 if a[1] == '남자' and a[4] == reg[i]:
-                                    print("Pulling {} from temp...".format(a[0]))
+                                    self.textBrowser.append("Pulling {} from temp...".format(a[0]))
                                     regions[i].append(temp.pop(temp.index(a)))
                                     isChanged = 1
                             else:
                                 if a[1] == '여자' and a[4] == reg[i]:
-                                    print("Pulling {} from temp...".format(a[0]))
+                                    self.textBrowser.append("Pulling {} from temp...".format(a[0]))
                                     regions[i].append(temp.pop(temp.index(a)))
                                     isChanged = 1
                     if not isChanged:
-                        print('Nobody matches on temporary list, continue...')
+                        self.textBrowser.append('Nobody matches on temporary list, continue...')
             if not isChanged or _ > 100:
                 break
-
+        self.progressBar.setProperty("value", 90)
         wb = openpyxl.Workbook()
         ws = wb.active
         ws.title = '임시명단'
@@ -216,15 +238,24 @@ class Aum(QWidget):
             for r in range(len(regions[i])):
                 for k in range(len(regions[i][r])):
                     ws.cell(row=r + 3, column=k + 1).value = regions[i][r][k]
+        self.progressBar.setProperty("value", 100)
 
         wb.save(r'aumpeople_sorted.xlsx')
-        print("\n" * 20 + "======" * 10)
-        print("자동 배치가 완료되었습니다.\n\n"
-              "해당 배치는 엑셀 순번에 따라 정원수를 기준으로 작성되었습니다. \n\n최종 합격자 명단은 아닙니다.\n\n"
-              "임시 정원외 명단으로 배치된 경우는 :\n\n1. 그룹의 정원 초과\n2. 그룹 내 성비 조정\n\n"
-              "으로 자동 조정된 것이며, 자동배치이므로 임시 정원외 명단도 확인해 주세요.\n\n" + "======" * 10)
-        if input("같은 폴더에 aumpeople_sorted.xlsx로 저장되었습니다. 엔터키를 눌러 종료하십시오."):
-            exit()
+        self.exitButton.setEnabled(True)
+
+        self.textBrowser.append("\n" * 20 + "======" * 10)
+        self.textBrowser.append("자동 배치가 완료되었습니다.\n"
+              "해당 배치는 엑셀 순번에 따라 정원수를 기준으로 작성되었습니다. \n최종 합격자 명단은 아닙니다.\n"
+              "임시 정원외 명단으로 배치된 경우는 :\n1. 그룹의 정원 초과\n2. 그룹 내 성비 조정\n"
+              "으로 자동 조정된 것이며, 자동배치이므로 임시 정원외 명단도 확인해 주세요.\n" + "======" * 10)
+        self.textBrowser.append("같은 폴더에 aumpeople_sorted.xlsx로 저장되었습니다.")
+        self.exitButton.setEnabled(True)
+        return 0
+
+    def openFile(self):
+        import os
+        os.startfile('aumpeople_sorted.xlsx')
+        return 0
 
 
 if __name__ == "__main__":
@@ -235,4 +266,3 @@ if __name__ == "__main__":
     ui.setupUi(Form)
     Form.show()
     sys.exit(app.exec_())
-
