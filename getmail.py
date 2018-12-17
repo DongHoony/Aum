1980cedf707dd6022a875dc217d557ad539bb60f
DongHoony <31026350+DongHoony@users.noreply.github.com>
2018-12-17 23:07:30 +0900

    Create getmail.py

---
+++ getmail.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+import openpyxl
+from time import sleep
+driver = webdriver.Chrome('chromedriver.exe')
+
+driver.get('https://nid.naver.com/nidlogin.login')
+
+
+input("Enter if you're ready")
+
+from bs4 import BeautifulSoup
+
+driver.get("https://mail.naver.com")
+
+file = openpyxl.load_workbook('mail_sort.xlsx')
+sheet = file.active
+
+
+try:
+    for pages in range(8):
+        for buttons in range(10):
+            sleep(0.3)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'lxml')
+            title_list = soup.find_all('strong', 'mail_title')
+            from_list = soup.find_all('div', 'mTitle')
+            date = soup.find_all('li', 'iDate')
+            for i in range(30):
+                print(str(from_list[i].find('a').get('title')).replace("\"", "", 2).replace(">","").replace(" <", ": "))
+                print(title_list[i].text)
+                sheet.cell(row=pages*300+buttons*30+i+1, column=1).value=str(from_list[i].find('a').get('title')).replace("\"", "", 2).replace(">","").replace(" <", ": ")
+                sheet.cell(row=pages*300+buttons*30+i+1, column=2).value=title_list[i].text
+                sheet.cell(row=pages * 300 + buttons * 30 + i + 1, column=3).value = date[i].text
+            if buttons != 9:
+                driver.find_element_by_xpath('//*[@id="{}"]'.format(pages*10+(buttons+2))).click()
+                sleep(0.5)
+        driver.find_element_by_xpath('//*[@id="next_page"]').click()
+        sleep(0.5)
+except IndexError:
+    file.save('mail_sorted.xlsx')
+    exit()
+file.save('mail_sorted.xlsx')
+exit()
